3cc93b77f9c77f6f226ebb8002b1a96a5d496ab6
andrelandgraf <andre.timo.landgraf@gmail.com>
2018-05-27 10:21:11 +0200

    config js - managing the backend base url globally in one file.

---
+++ src/config.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//Configuration variables
+const port      = process.env.PORT        || '3001';
+const backendUrl   = 'http://localhost:3000';
+
+module.exports = {
+    port,
+    backendUrl
+};
