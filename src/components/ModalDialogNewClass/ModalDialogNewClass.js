090bf53d1522fcb173004659f7aab2c0a8a007af
Hermann Grübel <>
2018-05-27 21:12:57 +0200

    Put state from CreateExercise into ModalDialogNewHomework --> but still a lot of stuff is missing!

--- src/components/ModalDialogNewClass/ModalDialogNewClass.js
+++ src/components/ModalDialogNewClass/ModalDialogNewClass.js
@@ -4,7 +4,6 @@ import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogActions from '@material-ui/core/DialogActions';
 
 import './ModalDialogNewClass.css';
