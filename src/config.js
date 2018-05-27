ffcd02cc0c7b2dfdb8348c674d54bcaf37f3f4cd
andrelandgraf <andre.timo.landgraf@gmail.com>
2018-05-27 10:26:54 +0200

    Port changed to 3000 for frontend

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 "use strict";
 
 //Configuration variables
-const port      = process.env.PORT        || '3001';
-const backendUrl   = 'http://localhost:3000';
+const port      = process.env.PORT        || '3000';
+const backendUrl   = 'http://localhost:3001';
 
 module.exports = {
     port,
