9c25358a961f142447744c68e83f116180460daf
andrelandgraf <andre.timo.landgraf@gmail.com>
2018-05-27 10:32:57 +0200

    config js - managing the backend base url globally in one file.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, {Component} from 'react';
-import Page from './components/Page/Page'
-import {ClassListView} from "./views/ClassListView";
 import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import blue from '@material-ui/core/colors/blue'
+
+import Page from './components/Page/Page'
+import {ClassListView} from './views/ClassListView';
+import UserService from './services/UserService';
 
 class App extends Component {
 
@@ -16,7 +18,8 @@ class App extends Component {
 
         });
 
-        return (
+        if(UserService.isAuthenticated()){
+            return (
 
                 <MuiThemeProvider theme={theme}>
                     <Page>
@@ -24,7 +27,21 @@ class App extends Component {
                     </Page>
                 </MuiThemeProvider>
 
-        );
+            );
+        }else{
+            // for debugging purposes. Later return LandingPage here.
+            return (
+
+                <MuiThemeProvider theme={theme}>
+                    <Page>
+                        <ClassListView/>
+                    </Page>
+                </MuiThemeProvider>
+
+            );
+        }
+
+
     }
 }
 
