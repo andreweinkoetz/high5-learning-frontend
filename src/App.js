8fd8b51fb9f499338b7aaae92b20118255db0b67
andrelandgraf <andre.timo.landgraf@gmail.com>
2018-05-27 11:05:50 +0200

    Ref #13 paramter userId will be forwarded now.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import blue from '@material-ui/core/colors/blue'
 import Page from './components/Page/Page'
 import {ClassListView} from './views/ClassListView';
 import UserService from './services/UserService';
+import LandingPage from './components/LandingPage/LandingPage';
 
 class App extends Component {
 
@@ -19,11 +20,13 @@ class App extends Component {
         });
 
         if(UserService.isAuthenticated()){
+            const user = UserService.getCurrentUser();
+            const id = user.id;
             return (
 
                 <MuiThemeProvider theme={theme}>
                     <Page>
-                        <ClassListView/>
+                        <ClassListView userId={id}/>
                     </Page>
                 </MuiThemeProvider>
 
@@ -31,13 +34,11 @@ class App extends Component {
         }else{
             // for debugging purposes. Later return LandingPage here.
             return (
-
                 <MuiThemeProvider theme={theme}>
                     <Page>
-                        <ClassListView/>
+                        <ClassListView userId={id}/>
                     </Page>
                 </MuiThemeProvider>
-
             );
         }
 
