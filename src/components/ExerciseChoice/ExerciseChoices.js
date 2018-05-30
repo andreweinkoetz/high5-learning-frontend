8a594a8b05877a012917836f02f0df9876703660
ge98hod <mflindemann@googlemail.com>
2018-05-30 11:05:08 +0200

    Introduction of Grid System to ExerciseChoices so that it can be easily reused for display of solution and statistics
    Added material icons to library

--- src/components/ExerciseChoice/ExerciseChoices.js
+++ src/components/ExerciseChoice/ExerciseChoices.js
@@ -9,6 +9,8 @@ import Typography from '@material-ui/core/Typography';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import FormLabel from '@material-ui/core/FormLabel';
 import FormControl from '@material-ui/core/FormControl';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 import './ExerciseChoices.css';
 
@@ -26,43 +28,103 @@ export class ExerciseChoices extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                <ExpansionPanel>
-                    <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
-                        <Typography className={"questionExercise"}>{this.state.question}</Typography>
-                    </ExpansionPanelSummary>
-                    <ExpansionPanelDetails>
+        return (<div>
+            {/*<ExpansionPanel>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+                    <Typography className={"questionExercise"}>{this.state.question}</Typography>
+                </ExpansionPanelSummary>
+                <ExpansionPanelDetails>
 
-                        <FormControl>
-                            <FormLabel>Choose the correct answer:</FormLabel>
+                    <FormControl>
+                        <FormLabel>Choose the correct answer:</FormLabel>
 
-                            <RadioGroup
-                                //onChange={this.props.changeRadio(this.state.id)}
-                                row="true"
-                            >
-                                <FormControlLabel value={this.state.answers[0]} control={<Radio/>}
-                                                  label={this.state.answers[0]}/>
+                        <RadioGroup
+                            //onChange={this.props.changeRadio(this.state.id)}
+                            row="true"
+                        >
+                            <FormControlLabel value={this.state.answers[0]} control={<Radio/>}
+                                              label={this.state.answers[0]}/>
+                            <i className="material-icons">
+                                check_circle
+                            </i>
 
-                                <FormControlLabel value={this.state.answers[1]} control={<Radio/>}
-                                                  label={this.state.answers[1]}/>
-                            </RadioGroup>
-                            <RadioGroup
-                                //onChange={this.props.changeRadio(this.state.id)}
-                                row="true"
-                            >
-                                <FormControlLabel value={this.state.answers[2]} control={<Radio/>}
-                                                  label={this.state.answers[2]}/>
+                            <FormControlLabel value={this.state.answers[1]} control={<Radio/>}
+                                              label={this.state.answers[1]}/>
+                        </RadioGroup>
+                        <RadioGroup
+                            //onChange={this.props.changeRadio(this.state.id)}
+                            row="true"
+                        >
+                            <FormControlLabel value={this.state.answers[2]} control={<Radio/>}
+                                              label={this.state.answers[2]}/>
 
-                                <FormControlLabel value={this.state.answers[3]} control={<Radio/>}
-                                                  label={this.state.answers[3]}/>
+                            <FormControlLabel value={this.state.answers[3]} control={<Radio/>}
+                                              label={this.state.answers[3]}/>
 
-                            </RadioGroup>
-                        </FormControl>
-                    </ExpansionPanelDetails>
-                </ExpansionPanel>
+                        </RadioGroup>
+                    </FormControl>
+                </ExpansionPanelDetails>
+            </ExpansionPanel>*/}
 
-            </div>);
+            <ExpansionPanel>
+                <ExpansionPanelSummary expandIcon={<ExpandMoreIcon/>}>
+                    <Typography className={"questionExercise"}>{this.state.question}</Typography>
+                </ExpansionPanelSummary>
+
+                <Grid container spacing={0}>
+                    <Grid item xs={8} sm={4}>
+                        <FormControlLabel value={this.state.answers[0]} control={<Radio/>}
+                                          label={this.state.answers[0]}/>
+                    </Grid>
+                    <Grid item xs={2} sm={1}>
+                        <i className="material-icons">
+                            check_circle
+                        </i>
+                    </Grid>
+                    <Grid item xs={2} sm={1}>
+                        <p>50%</p>
+                    </Grid>
+                    <Grid item xs={8} sm={4}>
+                        <FormControlLabel value={this.state.answers[1]} control={<Radio/>}
+                                          label={this.state.answers[1]}/>
+                    </Grid>
+                    <Grid item xs={2} sm={1}>
+                        <i className="material-icons">
+                            cancel
+                        </i>
+                    </Grid>
+                    <Grid item xs={2} sm={1}>
+                        <p>40%</p>
+                    </Grid>
+                    <Grid item xs={8} sm={4}>
+                        <FormControlLabel value={this.state.answers[2]} control={<Radio/>}
+                                          label={this.state.answers[2]}/>
+                    </Grid>
+                    <Grid item xs={2} sm={1}>
+                        <i className="material-icons">
+                            cancel
+                        </i>
+                    </Grid>
+                    <Grid item xs={2} sm={1}>
+                       <p>5%</p>
+                    </Grid>
+                    <Grid item xs={8} sm={4}>
+                        <FormControlLabel value={this.state.answers[3]} control={<Radio/>}
+                                          label={this.state.answers[3]}/>
+                    </Grid>
+                    <Grid item xs={2} sm={1}>
+                        <i className="material-icons">
+                            cancel
+                        </i>
+                    </Grid>
+                    <Grid item xs={2} sm={1}>
+                        <p>5%</p>
+                    </Grid>
+                </Grid>
+            </ExpansionPanel>
+
+
+        </div>);
     }
 }
 
