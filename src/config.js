d90572de05eb2b99b234a8a00a12ccc9c49d89a8
Andre Weinkoetz <andre.weinkoetz@tum.de>
2018-06-12 12:03:08 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 //Configuration variables
 const port      = process.env.PORT        || '3000';
-const backendUrl   = 'http://localhost:3001';
-const baseUrlFrontend = 'http://localhost:'+port;
+const backendUrl   = process.env.backendUrl || 'http://localhost:3001';
+const baseUrlFrontend = process.env.ownUrl || 'http://localhost:'+port;
 
 module.exports = {
     port,
