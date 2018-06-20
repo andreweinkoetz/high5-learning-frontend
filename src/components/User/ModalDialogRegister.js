cb117a614b85799cd548379b639141d8a3b3a4cd
andre <andre.weinkoetz@tum.de>
2018-06-20 09:39:33 +0200

    #50 added ignore eslint line for regex as they are valid according to https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Global_Objects/RegExp

--- src/components/User/ModalDialogRegister.js
+++ src/components/User/ModalDialogRegister.js
@@ -82,6 +82,8 @@ class ModalDialogRegister extends Component {
             });
             return;
         }
+        
+        //eslint-disable-next-line
         let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
         if (!re.test(username)) {
